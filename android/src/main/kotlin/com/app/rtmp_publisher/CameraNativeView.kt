0d0e9d704e819a27958f8371f18179cdca41f1c7
Emilio Dalla Torre <info@emiliodallatorre.it>
2021-05-10 17:20:21 +0200

    Commented two errors.

--- android/src/main/kotlin/com/app/rtmp_publisher/CameraNativeView.kt
+++ android/src/main/kotlin/com/app/rtmp_publisher/CameraNativeView.kt
@@ -53,11 +53,11 @@ class CameraNativeView(
     }
 
     override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+        // TODO("Not yet implemented")
     }
 
     override fun surfaceDestroyed(p0: SurfaceHolder) {
-        TODO("Not yet implemented")
+        // TODO("Not yet implemented")
     }
 
     override fun onAuthSuccessRtmp() {
